4a48c72d9189cef8a6a12eebbca3d1458e607bc0
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-05-27 18:00:38 +0800

    Update `color.c`
    Add comments like color text

--- src/color.c
+++ src/color.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include "color.h"
 
+// Red text
 void red() {
   printf("\033[1;31m");
 }
 
+// Green text
 void green() {
   printf("\033[1;32m");
 }
 
+// Yellow text
 void yellow() {
   printf("\033[1;33m");
 }
 
+// Blue text
 void blue() {
   printf("\033[1;34m");
 }
 
+// Purple text
 void purple() {
   printf("\033[1;35m");
 }
 
+// White text
 void white() {
   printf("\033[1;37m");
 }
